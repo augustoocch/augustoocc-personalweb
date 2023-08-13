d78a73a3c5612aa5ece47acef76fbec2100aca16
Augusto Occhiuzzi <augustoocchiuzzi@Augustos-Air.fibertel.com.ar>
2023-08-13 16:41:42 -0300

    Changed foother image

--- src/components/layouts/Foother.jsx
+++ src/components/layouts/Foother.jsx
@@ -13,16 +13,16 @@ function Footer() {
                 </div>
             </NavLink>
             <div className="flex-item">
-                <a href="https://github.com/augustoocch" target="_BLANK" >
-                    <img className="imagenLinkedin mr-5"alt="Github" src="/src/assets/images/github.png"/>
+                <a href="https://github.com/augustoocch " target="_BLANK" >
+                    <img className="imagenFoot mr-5" alt="Instagram" src="https://www.nicepng.com/png/full/52-520535_free-files-github-github-icon-png-white.png"/>
                 </a>
                 
                 <a href="https://www.instagram.com/augustoocchiuzzi/" target="_BLANK" >
-                    <img className="imagenInsta mr-5" alt="Instagram" src="https://www.pngkit.com/png/full/85-856104_new-instagram-logo-transparent-related-keywords-logo-instagram.png"/>
+                    <img className="imagenFoot mr-5" alt="Instagram" src="https://www.pngkit.com/png/full/85-856104_new-instagram-logo-transparent-related-keywords-logo-instagram.png"/>
                 </a>
 
                 <a href="https://www.linkedin.com/in/augusto-occhiuzzi-developer/" target="_BLANK" >
-                    <img className="imagenLinkedin"alt="LinkedIn" src="https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg"/>
+                    <img className="imagenFoot"alt="LinkedIn" src="https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg"/>
                 </a>
             </div>
         </div>
@@ -32,3 +32,7 @@ function Footer() {
 }
 
 export default Footer;
+
+
+//<i className="fa-brands fa-github mr-5" style={{ color: '#ffffff', fontSize: '25px', alignContent: 'left' }}></i>
+               
