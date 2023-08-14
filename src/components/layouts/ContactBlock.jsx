bbb615332e447d1c290a365f773500fb72580546
augustoocch <augusto.occh@gmail.com>
2023-08-14 11:15:37 -0300

    Added form validation

--- src/components/layouts/ContactBlock.jsx
+++ src/components/layouts/ContactBlock.jsx
@@ -3,7 +3,43 @@ import React, { useState, useEffect, useRef } from 'react';
 function ContactBlock() {
 
 
+    const [formData, setFormData] = useState({
+        name: '',
+        surname: '',
+        email: '',
+        comment: ''
+    });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }));
+    };
+
+    const [formErrors, setFormErrors] = useState({});
+
+    const handleValidations = (e) => {
+        e.preventDefault();
+
+        const errors = {};
+
+        // Validate empty fields
+        if (isBlank(formData.name) || isBlank(formData.surname) || isBlank(formData.email) || isBlank(formData.comment)) {
+            errors.error = "All fields are required";
+        }
+
+        // If there are errors, update the state and return
+        if (Object.keys(errors).length > 0) {
+            setFormErrors(errors);
+            return;
+        }
+    };
+
+    function isBlank(str) {
+        return (!str || /^\s*$/.test(str));
+    }
 
     return(
 
@@ -17,27 +53,29 @@ function ContactBlock() {
             </div>
             <div>
                 <div className="container_form2">
-     
-                    <form action="https://formsubmit.co/augusto.occh@gmail.com" method="POST">
-                        <div className="form-group">
-                            <input type="text" name="name" className="form-control animate__animated animate__fadeIn nombre" id="nombre" placeholder="Name" />
+                    {formErrors.error && (
+                        <div className="error-block text-center">
+                            <img src="/images/warning.webp"/>
+                            <p>{formErrors.error}</p>
                         </div>
-                        <div className="form-group">
-                            <input type="text" name="surname" className="form-control animate__animated animate__fadeIn nombre" id="apellido" placeholder="Surname"  />
-                        </div>
-                        <div className="form-group">
-                            <input type="email" name="email" className="form-control animate__animated animate__fadeIn nombre" id="email" placeholder="Email" />
-                        </div>
-        
-                        <div className="form-group">
-                            <textarea type="text" name="coment" className="form-control animate__animated animate__fadeIn nombre" id="coment"
-                                aria-describedby="emailHelp" placeholder="Send your comment"></textarea>
-                        </div>
-
-                        <div className="button-block">
-                            <button type="submit" className="btn btn-primary animate__animated animate__fadeIn">Send</button>
-                        </div>
-                    </form>
+                    )}
+                   <form onSubmit={handleValidations} method="POST">
+                    <div className="form-group">
+                        <input type="text" name="name" value={formData.name} onChange={handleChange} className="form-control animate__animated animate__fadeIn nombre" id="nombre" placeholder="Name" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="surname" value={formData.surname} onChange={handleChange} className="form-control animate__animated animate__fadeIn nombre" id="apellido" placeholder="Surname" />
+                    </div>
+                    <div className="form-group">
+                        <input type="email" name="email" value={formData.email} onChange={handleChange} className="form-control animate__animated animate__fadeIn nombre" id="email" placeholder="Email" />
+                    </div>
+                    <div className="form-group">
+                        <textarea type="text" name="comment" value={formData.comment} onChange={handleChange} className="form-control animate__animated animate__fadeIn nombre" id="coment" aria-describedby="emailHelp" placeholder="Send your comment"></textarea>
+                    </div>
+                    <div className="button-block">
+                        <button type="submit" className="btn btn-primary animate__animated animate__fadeIn">Send</button>
+                    </div>
+                </form>
                 </div>
             </div>
         </div>
